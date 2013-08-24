6183465544881c20a916ed70a5f926463ebb2715
Nicholas Sherlock <n.sherlock@gmail.com>
2013-08-24 03:13:36 +0000

    Node enumeration code is looking really tidy!

--- src/backup.js
+++ src/backup.js
@@ -1,149 +1,315 @@
-YUI.add('ss-smugmug-node-enumerator', function(Y, NAME) {
-	Y.SmugMugNodeEnumerator = Y.Base.create(
-		NAME,
+YUI.add('ss-smugmug-tools', function(Y) {
+	var SmugmugTools = Y.Base.create(
+		'ssSmugmugTools',
+		Y.Base,
+		[],
+		{
+		},
+		{
+			/**
+			 * Nickname is the SmugMug site nickname to look up, the ID is returned
+			 * asynchronously.
+			 * 
+			 * Provide success() and failure() functions in 'on' to receive the result.
+			 * 
+			 * @param nickname
+			 * @param on
+			 */
+			getRootNodeID: function(nickname, on) {
+				Y.io('http://' + nickname + '.smugmug.com/services/api/json/1.4.0/', {
+					data: {
+						disableAlbum:1,
+						disableEmpty:1,
+						disablePages:1,
+						nickname : nickname,
+						type:"view-folder",
+						method:"rpc.thumbnail.folders"
+					},
+					on: {
+						success: function(transactionid, response, arguments) {
+							var data = Y.JSON.parse(response.responseText);
+							
+							if (data && data.folderNodeId) {
+								on.success(data.folderNodeId);
+							} else {
+								on.failure();
+							}
+						},
+						failure: function(transactionid, response, arguments) {
+							on.failure();
+						}
+					}
+				});	
+			}			
+		}
+	);
+	
+	Y.namespace('SherlockPhotography').SmugmugTools = SmugmugTools;
+}, '0.0.1', {
+	requires: ['base', 'io']
+});
+
+YUI.add('ss-request-delay-queue', function(Y) {
+	
+	/**
+	 * Subscribe to the 'completed' event to know when all outstanding requests have been completed.
+	 */
+	var RequestDelayQueue = Y.Base.create(
+		'requestDelayQueue',
 		Y.Base,
 		[],
 		{
 			//Instance members
-			_numSuccessfulRequests: 0,
-			_numFailedRequests: 0,
-			_numOutstandingRequests: 0,
-			_nodes: {},
+			_queue: null,
 
-			_queueNodeFetch: function(url, maxDepth, onCompletion) {
-				this._numOutstandingRequests++;
+			/**
+			 * Add a callback to be called at a later time.
+			 * 
+			 * @param fn Callback
+			 * @param context Context to be used in call to callback
+			 * @param args Array of arguments that will be separately provided to the callback
+			 */
+			enqueue: function(fn, context, args) {
+				this.set('numOutstandingRequests', this.get('numOutstandingRequests') + 1);
 				
-				this.get('workQueue').add({
-					fn: this._recursivelyFetchNodes,
-					context: this,
-					args: [url, maxDepth, onCompletion],
+				this._queue.add({
+					fn: fn,
+					context: context,
+					args: args,
 					timeout: this.get('delayBetweenNodes')
 				}).run();
 			},
 			
-			_nodeFetchCompleted: function(success, onCompletion) {
-				this._numOutstandingRequests--;
+			/**
+			 * Add a queued item back onto the queue (i.e. doesn't increase the number of
+			 * outstanding requests, the item is still outstanding)
+			 * 
+			 * @param fn Callback
+			 * @param context Context to be used in call to callback
+			 * @param args Array of arguments that will be separately provided to the callback
+			 */
+			retry: function(fn, context, args) {
+				this._queue.add({
+					fn: fn,
+					context: context,
+					args: args,
+					timeout: this.get('delayBetweenNodes')
+				}).run();
+			},			
+			
+			itemCompleted: function(success) {
+				this.set('numOutstandingRequests', this.get('numOutstandingRequests') - 1);
 				
 				if (success) {
-					this._numSuccessfulRequests++;
+					this.set('numSuccessfulRequests', this.get('numSuccessfulRequests') + 1);
 				} else {
-					this._numFailedRequests++;
+					this.set('numFailedRequests', this.get('numFailedRequests') + 1);
 				}
 				
-				if (this._numOutstandingRequests == 0) {
-					onCompletion(this._nodes);
+				if (this.get('numOutstandingRequests') == 0) {
+					this.fire("completed");
 				}
 			},
 			
-			/* Extract the initial page data provided to  Y.SM.Page.init() */
-			_extractPageData: function(source) {
-				var matches = source.match(/^\s*Y\.SM\.Page\.init\((.+)\);$/m);
+		    initializer : function(cfg) {
+		    	this._queue = new Y.AsyncQueue();
+		    },			
+		}, {	
+			ATTRS: {
+				//Delay in milliseconds between node fetches (be kind to SmugMug!)
+				delayBetweenNodes: {
+					value: 400
+				},
 				
-				if (matches) {
-					return Y.JSON.parse(matches[1]);
-				}
+				numFailedRequests: {
+					value: 0
+				},
+
+				numSuccessfulRequests: {
+					value: 0
+				},
 				
-				return false;
+				numOutstandingRequests: {
+					value: 0
+				},
+				
+				/* Number of requests that have finished (succeeded or failed */
+				numCompletedRequests: {
+					getter:function() {
+						return this.get('numFailedRequests') + this.get('numSuccessfulRequests'); 
+					}
+				},
+				
+				/* Sum of number of completed and number of pending requests */
+				numTotalRequests: {
+					getter:function() {
+						return this.get('numCompletedRequests') + this.get('numOutstandingRequests'); 
+					}
+				}				
+			}
+		}
+	);
+	
+	Y.namespace('SherlockPhotography').RequestDelayQueue = RequestDelayQueue;
+}, '0.0.1', {
+	requires: ['base', 'async-queue']
+});
+
+YUI.add('ss-smugmug-node-enumerator', function(Y, NAME) {
+	/**
+	 * Events: progress, completed
+	 */
+	var SmugmugNodeEnumerator = Y.Base.create(
+		NAME,
+		Y.Base,
+		[],
+		{
+			//Prototype members
+			_nodes: null,
+			
+			_reportProgress: function() {
+				this.fire('progress', {completed: this._workQueue.get('numCompletedRequests'), total: this._workQueue.get('numTotalRequests')});
 			},
 			
-			_recursivelyFetchNodes: function(url, maxDepth, onCompletion) {
+			_recursivelyFetchNodes: function(rootNode, maxDepth, retryCount) {
 				if (maxDepth <= 0) {
-					this._nodeFetchCompleted(false, onCompletion);
+					this._workQueue.itemCompleted(false);
 					
 					return;
 				}
 				
-				Y.io(url, {
+				Y.io('http://' + this.get('domain') + '/services/api/json/1.4.0/', {
+					data: {
+						NodeID: rootNode,
+						PageSize: 1000,
+						method: 'rpc.node.getchildnodes'
+					},
 					on: {
 						success: function(transactionid, response, arguments) {
-							var pageData = this._extractPageData(response.responseText);
+							var data = JSON.parse(response.responseText);
 							
-							if (pageData) {
-								this._nodes[pageData.nodeId] = Y.merge(this._nodes[pageData.nodeId], {pageData: pageData});
+							for (var index in data.Nodes) {
+								var node = data.Nodes[index];
+								
+								var seenAlready = this._nodes[node.NodeID] !== undefined;
 
-								if (maxDepth > 1) {
-									//Now enumerate the children and recurse
-									Y.io('http://' + this.get('domain') + '/services/api/json/1.4.0/', {
-										data: {
-											NodeID: pageData.nodeId,
-											PageSize: 1000,
-											method: 'rpc.node.getchildnodes'
-										},
-										on: {
-											success: function(transactionid, response, arguments) {
-												var data = JSON.parse(response.responseText);
-												
-												for (var index in data.Nodes) {
-													var node = data.Nodes[index];
-													
-													if (node.NodeID && node.Url && this._nodes[node.NodeID] === undefined) {
-														this._nodes[node.NodeID] = {nodeData: node}; // Store the basic node information from rpc.node.getchildnodes
-														
-														this._queueNodeFetch(node.Url, maxDepth - 1, onCompletion);
-													}
-												}
-											},
-											end: function() {
-												/* 
-												 * Once we've determined how many children we have, and counted up those outstanding requests,
-												 * it's safe to mark this node as done.
-												 */
-												this._nodeFetchCompleted(true, onCompletion);
-											}
-										},
-										context: this
-									});
-								} else {
-									//We're not fetching our children due to hitting the depth limit
-									this._nodeFetchCompleted(false, onCompletion);
+								if (node.NodeID && !seenAlready) {
+									this._nodes[node.NodeID] = {nodeData: node}; // Store the basic node information from rpc.node.getchildnodes
+								
+									if (node.HasChildren) {
+										if (maxDepth <= 1) {
+											this.fire('failed', {method: "rpc.node.getchildnodes", NodeID: node.NodeID, status: 0, statusText: "Node has children, but children are beyond recursion limit, skipping children"});											
+										} else {
+											this._workQueue.enqueue(this._recursivelyFetchNodes, this, [node.NodeID, maxDepth - 1]);
+										}
+									}
 								}
-							} else {
-								//TODO signal an event
-								console.log("Failed to fetch " + url);
-
-								this._nodeFetchCompleted(false, onCompletion);
 							}
+							
+							this._workQueue.itemCompleted(true);
+							this._reportProgress();
 						},
-						failure: function() {
-							this._nodeFetchCompleted(false, onCompletion);
+						failure: function(transactionid, response, arguments) {
+							if (!retryCount) 
+								retryCount = 1; 
+							else
+								retryCount++;
+							
+							if (retryCount > this.get('maxRetries')) {
+								//All our retries failed, this node is failed
+								this._workQueue.itemCompleted(false);
+								this.fire('failed', {method: "rpc.node.getchildnodes", NodeID: rootNode, status: response.status, statusText: response.statusText});
+								
+								this._reportProgress();
+							} else {
+								//Try to fetch this node again later
+								this._workQueue.retry(this._recursivelyFetchNodes, this, [rootNode, maxDepth, retryCount]);
+							}
 						}
 					},
 					context: this
-				}); 
-			},			
+				});
+			},
 			
-			fetchNodes: function(onCompletion) {
-				this._queueNodeFetch('http://' + this.get('domain') + '/', 2, onCompletion);
+		    initializer : function(cfg) {
+		    	this._workQueue = new Y.SherlockPhotography.RequestDelayQueue();
+		    	this._nodes = {};
+		    	
+		    	var self = this;
+		    	
+		    	this._workQueue.on('completed', function() {
+		    		self._reportProgress();
+		    		
+		    		self.fire('completed', {nodes: self._nodes});
+		    	});
+		    },
+		    	
+			fetchNodes: function(rootNode) {
+				this._workQueue.enqueue(this._recursivelyFetchNodes, this, [rootNode, this.get('maxDepth')]);
 			}
 		}, {	
 			ATTRS: {
 				//SmugMug domain name we're fetching
 				domain : {},
 				
-				//Provide a Y.AsyncQueue to schedule AJAX execution
-				workQueue: {},
+				//Maximum depth to fetch (1 means just the root)
+				maxDepth: {
+					value: 2
+				},
 				
-				//Delay in milliseconds between node fetches (be kind to SmugMug!)
-				delayBetweenNodes: {
-					value: 400
+				//How many times will we try to fetch the same resource upon error?
+				maxRetries: {
+					value: 3
 				}
 			}
 		}
 	);
 	
+	Y.namespace('SherlockPhotography').SmugmugNodeEnumerator = SmugmugNodeEnumerator;
 }, '0.0.1', {
-	requires: ['io', 'base', 'json-parse']
+	requires: ['io', 'base', 'json-parse', 'ss-request-delay-queue']
 });
 
-YUI().use(['node', 'async-queue', 'io', 'ss-smugmug-node-enumerator'], function(Y) {
-	var 
-		smugmug_domain = 'n-sherlock.smugmug.com',
-		workQueue = new Y.AsyncQueue(),
-		nodeEnumerator = new Y.SmugMugNodeEnumerator({domain: smugmug_domain, workQueue: workQueue});
-
-	nodeEnumerator.fetchNodes(function(nodes) {
-		console.log(nodes);
-	});
+/* Extract the initial page data provided to  Y.SM.Page.init() */
+function extractPageData(source) {
+	var matches = source.match(/^\s*Y\.SM\.Page\.init\((.+)\);$/m);
 	
-	workQueue.run();
+	if (matches) {
+		return Y.JSON.parse(matches[1]);
+	}
+	
+	return false;
+}
+
+YUI().use(['node', 'json', 'io', 'ss-smugmug-tools', 'ss-smugmug-node-enumerator'], function(Y) {
+	Y.on('domready', function () {
+		var 
+			smugmugNickname = 'n-sherlock',
+			smugmugDomain = smugmugNickname + '.smugmug.com',
+			nodeEnumerator = new Y.SherlockPhotography.SmugmugNodeEnumerator({domain: smugmugDomain}),
+			progressDisplay = Y.Node.one('#progress');
+	
+		nodeEnumerator.on({
+			progress: function(progress) {
+				progressDisplay.set('text', progress.completed + '/' + progress.total);
+			},
+			failed: function(e) {
+				console.log("Request failed:");
+				console.log(e);
+			},
+			completed: function(e) {
+				console.log(e);
+			}
+		});
+		
+		//We must begin by finding out the ID of the root node of the domain:
+		Y.SherlockPhotography.SmugmugTools.getRootNodeID(smugmugNickname, {
+			success: function(rootNodeID) {
+				nodeEnumerator.fetchNodes(rootNodeID);
+			},
+			failure: function() {
+				alert("Couldn't find out the ID of the root node of your SmugMug site, are you logged on?");
+			}
+		});
+	});
 });
