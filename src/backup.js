8390d32ee6d87e47f3a4de790f3f76e852d5c23a
Nicholas Sherlock <n.sherlock@gmail.com>
2013-08-27 10:26:39 +0000

    Smugmug-treeview is running

--- src/backup.js
+++ src/backup.js
@@ -1,15 +1,19 @@
 YUI().use(['node', 'json', 'io', 'ss-smugmug-tools', 'ss-event-log-widget',  
-           'ss-progress-bar', 'ss-smugmug-site-backup'], function(Y) {
+           'ss-progress-bar', 'ss-smugmug-site-backup', 'ss-smugmug-backup-view'], function(Y) {
 	var 
 		eventLog = new Y.SherlockPhotography.EventLogWidget(),
 		
 		backup = new Y.SherlockPhotography.SmugmugSiteBackup({
 			smugmugNickname: 'n-sherlock',
 			eventLog: eventLog
+		}),
+		
+		backupView = new Y.SherlockPhotography.SmugmugBackupView({
 		});
 	
 	Y.on('domready', function () {
 		eventLog.render('#eventLog');
+		backupView.render('#backup-structure-pane');
 		
 		Y.one('#btn-backup-create').on('click', function(e) {
 			e.preventDefault();
--- src/lib/sherlockphotography/ss-smugmug-backup-view.js
+++ src/lib/sherlockphotography/ss-smugmug-backup-view.js
@@ -1,122 +1,31 @@
 YUI.add('ss-smugmug-backup-view', function(Y, NAME) {
+	var SmugmugBackupView = Y.Base.create(NAME, Y.Widget, [], {
+		_treeView : null,
 
-	var SmugmugBackupView = Y.Base.create(
-		'ss-event-log-entry',
-		Y.Base,
-		[],
-		{
-			_progressBar: null,
-			
-			_logTypeToClassname: function(type) {
-				var classname;
-				
-				switch (type) {
-					case 'error':
-					case 'warning':
-					case 'info':
-						classname = type;
-						break;
-					default:
-						classname = 'info'; 
-				}
-				
-				return 'log-' + classname;
-			},
-			
-			_uiSetProgress: function() {
-				var progress = this.get('progress');
-				
-				if (progress) {
-					if (!this._progressBar) {
-						this._progressBar = new Y.SherlockPhotography.ProgressBar(progress);
-						this._progressBar.render(this.get('element')); 
-					} else {
-						this._progressBar.set('total', progress.total);
-						this._progressBar.set('completed', progress.completed);
-					}
-				}
-			},
-			
-			_uiSetMessage: function() {
-				this.get('element').one('> .message').set('text', this.get('message'));
-			},
-			
-			render: function() {
-				element = Y.Node.create('<li class="' + this._logTypeToClassname(this.get('type')) + '"><span class="message"></span></li>');
-
-				this.set('element', element);
-
-				this._uiSetMessage();
-				this._uiSetProgress();
-				
-				return element;
-			},
-						
-			initializer: function(cfg) {
-				var self = this;
-				
-				this.after({
-					progressChange: function(e) {
-						self._uiSetProgress();
-					},
-					messageChange: function(e) {
-						self._uiSetMessage();
-					},					
-					elementChange: function(e) {
-						if (e.prevVal) {
-							e.prevVal.replace(e.newVal);
-						}
-					}
-				});
-				
-				this.render();
-			}
+		initializer : function(cfg) {
+			this._treeView = new Y.TreeView({
+				container : "#backup-structure-pane",
+				nodes : [ {
+					label : 'My Novel',
+					children : [ {
+						label : 'Chapter One'
+					}, {
+						label : 'Chapter Two'
+					} ]
+				} ]
+			});
 		},
-		{
-			ATTRS: {
-				element : {
-					value: null
-				},
-				type: {},
-				message: {},
-				progress: {
-					value: null
-				}
-			}
-		}
-	);
-	
-	var EventLogWidget = Y.Base.create(
-		NAME, 
-		Y.Widget, 
-		[], 
-		{
-			_list: null,
-			
-			renderUI: function() {
-				var contentBox = this.get("contentBox");
-				
-				this._list = Y.Node.create('<ul></ul>');
-				
-				contentBox.append(this._list);
-			},
-		
-			appendLog: function(type, message) {
-				var entry = new EventLogEntry({type:type, message:message});
-				
-				this._list.append(entry.get('element'));
-				
-				return entry;
-			}
+
+		renderUI : function() {
+			this._treeView.render();
 		},
-		{
-			ATTRS : {
-				
-			}
+	}, {
+		ATTRS : {
+
 		}
-	);
+	});
 
 	Y.namespace("SherlockPhotography").SmugmugBackupView = SmugmugBackupView;
 }, '0.0.1', {
-	requires : ['base', 'widget', 'escape']
+	requires : [ 'base', 'widget', 'gallery-sm-treeview', 'escape' ]
 });
