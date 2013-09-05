c69f671e838bf4f4c9c3b8ff54b4b3bbc61363ef
Nicholas Sherlock <n.sherlock@gmail.com>
2013-09-05 13:38:55 +0000

    Adding and removing PayPal button code basically works

--- src/paypal.js
+++ src/paypal.js
@@ -69,7 +69,27 @@ YUI().use(['node', 'json', 'io', 'event-resize', 'ss-event-log-widget',
 		queue.run();		
 	}
 	
-	function installPayPalButtons() {
+	var 
+		regPayPalItemNameField = /<input type="hidden" name="item_name" value="[^"]*">/,
+		regFindInstalledPayPalCode = /<div class="ss-paypal-button">[\s\S]+?<\/div>/, //Yeah, I know, Zalgo and all that.
+		regFindInstalledPayPalCodeGlobal = new RegExp(regFindInstalledPayPalCode.source, "g"); 
+	
+	/**
+	 * Does the PayPal button code look correct? 
+	 */
+	function validatePayPalCode(code) {
+		return code && code.match(regPayPalItemNameField);
+	}
+	
+	function preparePayPalCode(code) {
+		return '<div class="ss-paypal-button">' + code + '</div>';
+	}
+	
+	function customizePayPalCodeForImage(payPalCode, image) {
+		return payPalCode.replace(regPayPalItemNameField, '<input type="hidden" name="item_name" value="' + Y.Escape.html(image.WebUri) + '">');
+	}
+	
+	function installPayPalButtons(payPalCode) {
 		var 
 			nodes = Y.all(".smugmug-image.selected");
 
@@ -81,33 +101,32 @@ YUI().use(['node', 'json', 'io', 'event-resize', 'ss-event-log-widget',
 			logProgress = eventLog.appendLog('info', "Adding PayPal buttons to selected photos..."),
 			queue = new Y.SherlockPhotography.APISmartQueue({
 				processResponse: function(request, data) {
-					if (data.Code == 200 && data.Response) {
-						var response = data.Response;
-						
-						for (var index in response.AlbumImage) {
-							var image = response.AlbumImage[index];
-							
-							var rendered = Y.Node.create('<li><a class="smugmug-image" href="#" style="background-image: url(' + image.ThumbnailUrl + ')"></a></li>');
-							
-							rendered.setData('image', image);
-							
-							imageListContainer.append(rendered);
-						}
-					}
 					return true;
 				},
 				responseType: 'json',
 				delayBetweenRequests: 100
 			});
-
+		
 		nodes.each(function(node) {
 			var image = node.getData('image');
+			
+			var 
+				imagePayPalCode = customizePayPalCodeForImage(payPalCode, image),
+				newCaption;
+			
+			if (image.Caption.match(regFindInstalledPayPalCode)) {
+				newCaption = image.Caption.replace(regFindInstalledPayPalCode, imagePayPalCode);
+			} else if (image.Caption == ""){
+				newCaption = imagePayPalCode;
+			} else {
+				newCaption = image.Caption + "\n" + imagePayPalCode;
+			}
 			
 			queue.enqueueRequest({
 				url: 'http://' + smugDomain + image.Uris.Image.Uri + '?_method=PATCH',
 				method: 'POST',				
 				data: JSON.stringify({
-					Caption: image.Caption + "lol"
+					Caption: newCaption
 				}),
 				headers: {
 					'Accept': 'application/json',
@@ -131,6 +150,60 @@ YUI().use(['node', 'json', 'io', 'event-resize', 'ss-event-log-widget',
 		queue.run();
 	}
 	
+	function removePayPalButtons() {
+		var 
+			nodes = Y.all(".smugmug-image.selected");
+
+		if (!nodes.size()) {
+			return;
+		}
+		
+		var 
+			logProgress = eventLog.appendLog('info', "Removing PayPal buttons from selected photos..."),
+			queue = new Y.SherlockPhotography.APISmartQueue({
+				processResponse: function(request, data) {
+					return true;
+				},
+				responseType: 'json',
+				delayBetweenRequests: 100
+			});
+		
+
+		nodes.each(function(node) {
+			var image = node.getData('image');
+						
+			if (image.Caption.match(regFindInstalledPayPalCodeGlobal)) {
+				newCaption = image.Caption.replace(regFindInstalledPayPalCodeGlobal, '');
+			
+				queue.enqueueRequest({
+					url: 'http://' + smugDomain + image.Uris.Image.Uri + '?_method=PATCH',
+					method: 'POST',				
+					data: JSON.stringify({
+						Caption: newCaption
+					}),
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json'
+					}
+				});
+			}
+		});
+		
+		queue.on({
+			complete: function() {
+				logProgress.set('message', "Removing PayPal buttons from selected photos... done!");
+			},
+			requestFail: function(e) {
+				//eventLog._logError("Failed to fetch site themes");
+			},
+			progress: function(progress) {
+				logProgress.set('progress', progress);
+			}
+		});
+	
+		queue.run();
+	}	
+	
 	Y.on({
 		domready: function () {
 			eventLog.render('#eventLog');
@@ -145,11 +218,25 @@ YUI().use(['node', 'json', 'io', 'event-resize', 'ss-event-log-widget',
 
 			Y.one('#btn-apply').on({
 				click: function(e) {
-					installPayPalButtons();
+					var payPalCode = Y.one('#buynow-button-code').get('value');
+					
+					if (validatePayPalCode(payPalCode)) {
+						payPalCode = preparePayPalCode(payPalCode);
+						
+						installPayPalButtons(payPalCode);
+					}
 					
 					e.preventDefault();
 				}
 			});
+			
+			Y.one('#btn-remove').on({
+				click: function(e) {
+					removePayPalButtons();
+					
+					e.preventDefault();
+				}
+			});			
 			
 			enumeratePhotos();
 		}
