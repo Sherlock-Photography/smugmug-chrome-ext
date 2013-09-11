5cbb2adab78deb0a7b6e80e1d113a4076b5ed734
Nicholas Sherlock <n.sherlock@gmail.com>
2013-09-11 04:45:38 +0000

    Don't allow the option to add PayPal buttons if this isn't a gallery page

--- src/popup.js
+++ src/popup.js
@@ -6,12 +6,23 @@ document.addEventListener('DOMContentLoaded', function() {
 		
 		return false;
 	};
+
+	var buyNow = document.getElementById("paypal-buynow");
 	
-	document.getElementById("paypal-buynow").onclick = function() {
-		chrome.tabs.create({
-			url:'paypal.html'
-		});
-		
-		return false;
-	};	
+	if (chrome.extension.getBackgroundPage().pageDetails && chrome.extension.getBackgroundPage().pageDetails.userNode 
+			&& chrome.extension.getBackgroundPage().pageDetails.userNode.RemoteKey) {
+		buyNow.onclick = function() {
+			chrome.tabs.create({
+				url:'paypal.html'
+			});
+			
+			return false;
+		};
+	} else {
+		buyNow.parentNode.className = 'disabled';
+
+		buyNow.onclick = function() {
+			return false;
+		};
+	}
 });
