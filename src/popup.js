a747abdfdb659a3ec6a00f42ccdab4b4aa323419
Nicholas Sherlock <n.sherlock@gmail.com>
2014-01-03 01:06:14 +0000

    More noodling with pop up text

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 	
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {method:"getSiteDetail"}, function(siteDetail) {
 			document.getElementById("create-backup").onclick = function() {
 				chrome.tabs.create({
@@ -14,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 			// Is this a gallery page?
 			if (siteDetail.pageDetails && siteDetail.pageDetails.userNode && siteDetail.pageDetails.userNode.RemoteKey) {
-				
 				payPalButtons.onclick = function() {
 					chrome.tabs.create({
 						url: 'paypal.html?nickname=' + encodeURIComponent(siteDetail.nickname)
