6602d906e1599421119d3de01dfc514576fe6754
Nicholas Sherlock <n.sherlock@gmail.com>
2014-03-25 11:55:21 +0000

    Fix exception for pages that don't load sm-view-tiles and add more exception safety

--- src/tweaks/ss-organizer-collected.js
+++ src/tweaks/ss-organizer-collected.js
@@ -1,93 +1,97 @@
-YUI.add('ss-organizer-collected', function(Y) {	
-	var 
-		target = Y.SM.Organizer.Tiles,
-		Patch = function(){
-		};
+YUI.add('ss-organizer-collected', function(Y) {
+	try {
+		var 
+			target = Y.SM.Organizer.Tiles,
+			Patch = function(){
+			};
+			
+		var 
+			origRenderImageTile = target.prototype._renderImageTile,
+			newImageTileTemplate = Y.Template.Micro.compile(
+				'<li id="<%== data.id %>" class="sm-organizer-tile sm-organizer-tile-image<% if (data.tile.Origin == "Smart") { %> ss-tile-smart<% } %><% if (data.tile.Origin == "Collected") { %> ss-tile-collected<% } %><% if (data.tile.IsVideo) { %> sm-organizer-tile-video<% } %>">' +
+				
+				'<a id="<%== data.id %>-link" class="sm-organizer-tile-link" ' +
+				    'href="<%== data.url %>" style="height: <%== data.height %>; width: <%== data.width %>">' +
+				
+				    '<% if (data.icon) { %>' +
+				        '<div role="img" id="<%== data.id %>-image" ' +
+				            'class="sm-organizer-image sm-organizer-image-icon" ' +
+				            'aria-label="<%= data.title %>" ' +
+				            'style="height: <%== data.height %>; width: <%== data.width %>">' +
+				
+				            '<span class="sm-fonticon sm-fonticon-<%= data.icon %>"></span>' +
+				        '</div>' +
+				    '<% } else { %>' +
+				        '<div role="img" id="<%== data.id %>-image" ' +
+				            'class="sm-organizer-image sm-organizer-image-unloaded" ' +
+				            'aria-label="<% if (data.tile.IsVideo) { %>Video: <% } %><%= data.title %>" ' +
+				            'data-src="<%== data.imageUrl %>" ' +
+				            'style="height: <%== data.height %>; width: <%== data.width %>"></div>' +
+				
+				        '<% if (data.tile.IsVideo) { %>' +
+				            '<div role="presentation" class="sm-organizer-tile-icon ' +
+				                'sm-fonticon-sml sm-fonticon-VideoCamera"></div>' +
+				        '<% } else if (data.tile.IsVault) { %>' +
+				            '<div role="presentation" class="sm-organizer-tile-icon ' +
+				                'sm-fonticon-sml sm-fonticon-Vault"></div>' +
+				        '<% } %>' +
+				
+				        '<% if (data.isHidden || data.tile.Origin == "Smart"|| data.tile.Origin == "Collected") { %>' +
+				            '<div class="sm-organizer-image-overlay" ' +
+				                'style="height: <%== data.height %>; width: <%== data.width %>">' +
+				
+						        '<% if (data.isHidden) { %>' +
+						        	'<span class="sm-fonticon sm-fonticon-Hide"></span>' +
+						        '<% } else { %>' +
+					        		'<span class="sm-fonticon"></span>' +
+					        	'<% } %>' +
+				            '</div>' +
+				        '<% } %>' +
+				
+				    '<% } %>' +
+				'</a>' +
+				'</li>'
+			);
 		
-	var 
-		origRenderImageTile = target.prototype._renderImageTile,
-		newImageTileTemplate = Y.Template.Micro.compile(
-			'<li id="<%== data.id %>" class="sm-organizer-tile sm-organizer-tile-image<% if (data.tile.Origin == "Smart") { %> ss-tile-smart<% } %><% if (data.tile.Origin == "Collected") { %> ss-tile-collected<% } %><% if (data.tile.IsVideo) { %> sm-organizer-tile-video<% } %>">' +
-			
-			'<a id="<%== data.id %>-link" class="sm-organizer-tile-link" ' +
-			    'href="<%== data.url %>" style="height: <%== data.height %>; width: <%== data.width %>">' +
-			
-			    '<% if (data.icon) { %>' +
-			        '<div role="img" id="<%== data.id %>-image" ' +
-			            'class="sm-organizer-image sm-organizer-image-icon" ' +
-			            'aria-label="<%= data.title %>" ' +
-			            'style="height: <%== data.height %>; width: <%== data.width %>">' +
-			
-			            '<span class="sm-fonticon sm-fonticon-<%= data.icon %>"></span>' +
-			        '</div>' +
-			    '<% } else { %>' +
-			        '<div role="img" id="<%== data.id %>-image" ' +
-			            'class="sm-organizer-image sm-organizer-image-unloaded" ' +
-			            'aria-label="<% if (data.tile.IsVideo) { %>Video: <% } %><%= data.title %>" ' +
-			            'data-src="<%== data.imageUrl %>" ' +
-			            'style="height: <%== data.height %>; width: <%== data.width %>"></div>' +
-			
-			        '<% if (data.tile.IsVideo) { %>' +
-			            '<div role="presentation" class="sm-organizer-tile-icon ' +
-			                'sm-fonticon-sml sm-fonticon-VideoCamera"></div>' +
-			        '<% } else if (data.tile.IsVault) { %>' +
-			            '<div role="presentation" class="sm-organizer-tile-icon ' +
-			                'sm-fonticon-sml sm-fonticon-Vault"></div>' +
-			        '<% } %>' +
-			
-			        '<% if (data.isHidden || data.tile.Origin == "Smart"|| data.tile.Origin == "Collected") { %>' +
-			            '<div class="sm-organizer-image-overlay" ' +
-			                'style="height: <%== data.height %>; width: <%== data.width %>">' +
-			
-					        '<% if (data.isHidden) { %>' +
-					        	'<span class="sm-fonticon sm-fonticon-Hide"></span>' +
-					        '<% } else { %>' +
-				        		'<span class="sm-fonticon"></span>' +
-				        	'<% } %>' +
-			            '</div>' +
-			        '<% } %>' +
-			
-			    '<% } %>' +
-			'</a>' +
-			'</li>'
-		);
+		Patch.prototype._renderImageTile = function(tile) {
+	        if (window.sherlockPhotographySMForChrome.config.enable && window.sherlockPhotographySMForChrome.settings["collected-organiser"]
+	        	&& tile.data.Status !== 'Preprocess' && !tile.data.IsArchive) {
+	        	
+	        	var
+		        	tileSize = this.imageTileSize,
+		            sizeName = this.imageSize,
+		            size     = tile.data.Sizes[sizeName],
+		            height   = size.height,
+		            width    = size.width;
 	
-	Patch.prototype._renderImageTile = function(tile) {
-        if (window.sherlockPhotographySMForChrome.config.enable && window.sherlockPhotographySMForChrome.settings["collected-organiser"]
-        	&& tile.data.Status !== 'Preprocess' && !tile.data.IsArchive) {
-        	
-        	var
-	        	tileSize = this.imageTileSize,
-	            sizeName = this.imageSize,
-	            size     = tile.data.Sizes[sizeName],
-	            height   = size.height,
-	            width    = size.width;
-
-	        // Restrict the longest dimension to 100px.
-	        if (width > tileSize || height > tileSize) {
-	            if (width > height) {
-	                height = Math.floor((tileSize / width) * height);
-	                width  = tileSize;
-	            } else {
-	                width  = Math.floor((tileSize / height) * width);
-	                height = tileSize;
-	            }
+		        // Restrict the longest dimension to 100px.
+		        if (width > tileSize || height > tileSize) {
+		            if (width > height) {
+		                height = Math.floor((tileSize / width) * height);
+		                width  = tileSize;
+		            } else {
+		                width  = Math.floor((tileSize / height) * width);
+		                height = tileSize;
+		            }
+		        }
+		
+		        return newImageTileTemplate({
+		            height  : height + 'px',
+		            id      : tile.tileId,
+		            imageUrl: tile.imageUrl(sizeName),
+		            isHidden: tile.isHidden(),
+		            tile    : tile.data,
+		            title   : tile.name(),
+		            url     : tile.url(),
+		            width   : width + 'px'
+		        });        	
+	        } else {
+	        	return origRenderImageTile.call(this, tile);
 	        }
+	    };
 	
-	        return newImageTileTemplate({
-	            height  : height + 'px',
-	            id      : tile.tileId,
-	            imageUrl: tile.imageUrl(sizeName),
-	            isHidden: tile.isHidden(),
-	            tile    : tile.data,
-	            title   : tile.name(),
-	            url     : tile.url(),
-	            width   : width + 'px'
-	        });        	
-        } else {
-        	return origRenderImageTile.call(this, tile);
-        }
-    };
-
-	Y.augment(target, Patch, true);
+		Y.augment(target, Patch, true);
+	} catch (e) {
+		//Don't break SM if we fail
+	} 
 });
--- src/tweaks/ss-tile-base-view-collected.js
+++ src/tweaks/ss-tile-base-view-collected.js
@@ -1,59 +1,62 @@
-YUI.add('ss-tile-base-view-collected', function(Y) {	
-	var 
-		target = Y.SM.Views.TileBase.Shared,
-		Patch = function(){
-		};
-		
-	var origSetupRenderConfig = target.prototype._setupRenderConfig;
-		
-	Patch.prototype._setupRenderConfig = function(tile) {
-        origSetupRenderConfig.call(this, tile);
-
-        if (window.sherlockPhotographySMForChrome.config.enable && window.sherlockPhotographySMForChrome.settings["collected-thumbnail"] && this.get('showStatus')) {
-	        var imageModel = null;
-	        
-	        if (tile instanceof Y.SM.Models.Image) {
-	            imageModel = tile;
-	        } else if (tile.getRelated) {
-	            imageModel = tile.getRelated('Image');
-	        }
-	        
-	        if (imageModel) {
-	        	var 
-	        		clientId = tile.get('clientId'),
-	            	renderConfig = this._renderConfig[clientId],
-	            	tileClassNames = [];
+YUI.add('ss-tile-base-view-collected', function(Y) {
+	try {
+		var 
+			target = Y.SM.Views.TileBase.Shared,
+			Patch = function(){
+			};
+			
+		var origSetupRenderConfig = target.prototype._setupRenderConfig;
+			
+		Patch.prototype._setupRenderConfig = function(tile) {
+	        origSetupRenderConfig.call(this, tile);
 	
-	        	switch (imageModel.get('Origin')) {
-	        		case 'Smart':
-		        		tileClassNames.push('ss-tile-smart');
-		        		
-		        		renderConfig.showStatus = true;
-		        		break;
+	        if (window.sherlockPhotographySMForChrome.config.enable && window.sherlockPhotographySMForChrome.settings["collected-thumbnail"] && this.get('showStatus')) {
+		        var imageModel = null;
+		        
+		        if (tile instanceof Y.SM.Models.Image) {
+		            imageModel = tile;
+		        } else if (tile.getRelated) {
+		            imageModel = tile.getRelated('Image');
+		        }
+		        
+		        if (imageModel) {
+		        	var 
+		        		clientId = tile.get('clientId'),
+		            	renderConfig = this._renderConfig[clientId],
+		            	tileClassNames = [];
+		
+		        	switch (imageModel.get('Origin')) {
+		        		case 'Smart':
+			        		tileClassNames.push('ss-tile-smart');
+			        		
+			        		renderConfig.showStatus = true;
+			        		break;
+			        	
+		        		case 'Collected':
+			        		tileClassNames.push('ss-tile-collected');
+			        		
+			        		renderConfig.showStatus = true;
+			        		break;
+		        	}
 		        	
-	        		case 'Collected':
-		        		tileClassNames.push('ss-tile-collected');
-		        		
-		        		renderConfig.showStatus = true;
-		        		break;
-	        	}
-	        	
-	        	if (tileClassNames.length) {
-	        		renderConfig.tileClassNames = (renderConfig.tileClassNames ? renderConfig.tileClassNames + " " : "") + tileClassNames.join(' '); 
-	        	}
+		        	if (tileClassNames.length) {
+		        		renderConfig.tileClassNames = (renderConfig.tileClassNames ? renderConfig.tileClassNames + " " : "") + tileClassNames.join(' '); 
+		        	}
+		        }
 	        }
-        }
-    };
-
-	Y.mix(target, Patch, true, null, 1);
-
-	//Y.SM.Views.Tile might already have extended from Y.SM.Views.TileBase.Shared, so we need to patch that too:
-	if (Y.SM.Views.Tile) {
-		Y.mix(Y.SM.Views.Tile, Patch, true, null, 1);
-	}
+	    };
 	
-	if (Y.SM.Views.Tiles.Layout.Base) {
-		Y.mix(Y.SM.Views.Tiles.Layout.Base, Patch, true, null, 1);
-	}
+		Y.mix(target, Patch, true, null, 1);
 	
+		//Y.SM.Views.Tile might already have extended from Y.SM.Views.TileBase.Shared, so we need to patch that too:
+		if (Y.SM.Views.Tile) {
+			Y.mix(Y.SM.Views.Tile, Patch, true, null, 1);
+		}
+		
+		if (Y.SM.Views.Tiles && Y.SM.Views.Tiles.Layout && Y.SM.Views.Tiles.Layout.Base) {
+			Y.mix(Y.SM.Views.Tiles.Layout.Base, Patch, true, null, 1);
+		}
+	} catch (e) {
+		//Don't break SM if we fail
+	}
 });
