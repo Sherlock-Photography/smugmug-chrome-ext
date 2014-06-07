7cf0255a85ee5f3004a0f53b7553a5868400a429
Nicholas Sherlock <n.sherlock@gmail.com>
2014-06-07 07:04:16 +0000

    Remove link to old SmugMug bulk edit tool

--- src/popup.js
+++ src/popup.js
@@ -41,18 +41,6 @@ function show_popup(tab, hasPermission, domainName) {
 					return false;
 				};
 				
-				var bulkEdit = document.getElementById("bulk-edit");
-								
-				bulkEdit.onclick = function(e) {
-					chrome.tabs.create({
-						url: 'http://' + domainName[1] + '/photos/tools.mg?pageType=Album&tool=bulkcaption&AlbumID=' + encodeURIComponent(siteDetail.pageDetails.userNode.RemoteID)
-							+ '&AlbumKey=' + encodeURIComponent(siteDetail.pageDetails.userNode.RemoteKey)
-							+ '&url='+ encodeURIComponent(tab.url)
-					});
-					
-					return false;
-				};
-				
 				var bulkEditBeta = document.getElementById("bulk-edit-beta");
 				
 				bulkEditBeta.onclick = function(e) {
