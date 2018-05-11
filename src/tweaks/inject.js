c3308080be78f74b6e4c835cf438fe7e08ef09c2
Nicholas Sherlock <n.sherlock@gmail.com>
2018-05-12 09:49:55 +1200

    SmugMug now provides a "from..." link in the lightbox itself

--- src/tweaks/inject.js
+++ src/tweaks/inject.js
@@ -4,12 +4,6 @@
 			data = window.sherlockPhotographySMForChrome,
 			
 			modules = {
-				"ss-lightbox-view-collected": {
-				    path: "ss-lightbox-view-collected.js",
-				    condition: {
-				    	trigger: "sm-lightbox-view"
-				    }
-				},
 				"ss-tile-base-view-collected": {
 				    path: "ss-tile-base-view-collected.js",
 				    condition: {
@@ -31,12 +25,8 @@
 				}*/
 			};
 		
-		if (!window.YUI_config) {
-			window.YUI_config = {};
-		}
-		if (!window.YUI_config.groups) {
-			window.YUI_config.groups = {};
-		}
+		window.YUI_config = window.YUI_config || {};
+		window.YUI_config.groups = window.YUI_config.groups || {};
 		
 		window.YUI_config.groups["ss-smugmug-for-chrome"] = {
 			modules: modules,
